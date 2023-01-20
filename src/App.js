bcee71d0fb1b013259232b852d9281393fdadeda
omerkahraman <omerkahramaniletisim@gmail.com>
2023-01-20 09:40:40 +0300

    react router first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link, NavLink } from 'react-router-dom'
+import Home from './pages/Home'
+import Contact from './pages/Contact';
+import Blog from './pages/Blog';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+      <NavLink to="/" className={({isActive}) => isActive && 'aktif'}>Home Page</NavLink>
+      <NavLink to="/contact" style={({isActive}) => ({
+        backgroundColor: isActive ? 'red' : 'transparent'
+      })}>Contact</NavLink>
+      <NavLink to="/blog">
+        {({isActive}) => (
+          <>
+          Blog
+          {isActive && 'Aktif'}
+          </>
+        )}
+      </NavLink>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/blog' element={<Blog />} />
+      </Routes>
+    </>
   );
 }
 
