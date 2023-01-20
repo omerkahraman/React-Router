0ff162e19fc111fb0d222d8d8cb6c3aaeb971c91
omerkahraman <omerkahramaniletisim@gmail.com>
2023-01-20 10:17:03 +0300

    nested routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { Routes, Route, Link, NavLink } from 'react-router-dom'
 import Home from './pages/Home'
 import Contact from './pages/Contact';
-import Blog from './pages/Blog';
+import BlogLayout from './pages/blog';
+import Categories from './pages/blog/Categories';
+import Post from './pages/blog/Post';
+import Blog from './pages/blog/Blog';
 
 
 function App() {
@@ -23,7 +26,11 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/contact' element={<Contact />} />
-        <Route path='/blog' element={<Blog />} />
+        <Route path='/blog' element={<BlogLayout />}>
+          <Route index={true} element={<Blog />} />
+          <Route path='categories' element={<Categories />} />
+          <Route path='post/:url' element={<Post />} />
+        </Route>
       </Routes>
     </>
   );
